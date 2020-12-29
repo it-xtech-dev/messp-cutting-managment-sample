3104102a53ca9fa111ba91ec0634f57cf685152e
Paweł Kołodziej <pk@xtech.pl>
2020-12-29 09:43:01 +0100

    code update

--- messagepipe.js
+++ messagepipe.js
@@ -1,8 +1,10 @@
-function MessagePipe(targetWindow, targetOrigin) {
-  var onReceived = null
+function MessagePipe(targetWindow, targetOrigin, timeout) {
   var _sendQueue = [];
   var _isConnected = false;
   var _connectionTimer = null;
+  var _timeout = timeout || 5000;
+  var _connectedStartedOn = new Date();
+  var onReceived = null
   var onConnected = null;
 
   // Adding listener to target window (will listen for incomming messages);
@@ -12,10 +14,9 @@ function MessagePipe(targetWindow, targetOrigin) {
           if (event.data !== 'hello' && typeof onReceived === 'function') {
               // when non 'hello' message received process payload.
               onReceived(JSON.parse(event.data));
-          } else {
-              // when 'hello' message received set connected flag.
-              _isConnected = true;
           }
+          // when any message received set connected flag.
+          _isConnected = true;
       }
   }, false);
 
@@ -28,16 +29,20 @@ function MessagePipe(targetWindow, targetOrigin) {
   function _connect() {
       // awaits for window to be initialized by sending hello message
       _connectionTimer = setInterval(function () {
-          if (_isConnected) {
-              // when 'hello' message received from other side _isConnected flag will be set and connection process can be terminated.
-              clearInterval(_connectionTimer);
-              // all queued messges will be processed now as connection is establised:
-              for (var i = 0; i < _sendQueue.length; i++) {
-                  _sendNow(_sendQueue[i]);
-              }
+          if ((new Date()).getTime() - _connectedStartedOn.getTime() >= timeout) {
+            // when time out reached, throw an exception;
+            clearInterval(_connectionTimer);
+            throw('Pipe timeout exceeded!');
+          } else if (_isConnected) {
+            // when 'hello' message received from other side _isConnected flag will be set and connection process can be terminated.
+            clearInterval(_connectionTimer);
+            // all queued messges will be processed now as connection is establised:
+            for (var i = 0; i < _sendQueue.length; i++) {
+              _sendNow(_sendQueue[i]);
+            }
           } else {
-              // send 'hello' message until other side hello received.
-              _sendNow('hello');
+            // send 'hello' message until other side hello received.
+            _sendNow('hello');
           }
       }, 100);
   }
